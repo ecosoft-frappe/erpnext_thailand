199f452fd8f4ec81b4c31403543523025ddc215f
Kitti U. <kittiu@gmail.com>
2025-04-13 22:08:27 +0700

    Refactor JS by merged the common code.

--- erpnext_thailand/public/js/sales_invoice.js
+++ erpnext_thailand/public/js/sales_invoice.js
@@ -15,43 +15,7 @@ frappe.ui.form.on("Sales Invoice", {
         };
     },
 
-    get_deposits(frm, is_button_clicked = true) {
-        frappe.call({
-            method: "erpnext_thailand.custom.deposit_invoice.get_deposits",
-            args: { doc: frm.doc },
-            callback: function(r) {
-                // Clear existing deductions
-                frm.clear_table("deposits");
-                if (r.message.length > 0) {
-                    let deductions = r.message;
-                    let allocated_amount = 0;
-                    // Add new deductions
-                    deductions.forEach(function(d) {
-                        let c = frm.add_child("deposits");
-                        c.reference_name = d.reference_name
-                        c.reference_row = d.reference_row
-                        c.remarks = d.remarks
-                        c.deposit_amount = d.deposit_amount
-                        c.allocated_amount = d.allocated_amount
-                        // Keep track of the total allocated amount
-                        allocated_amount += d.allocated_amount
-                    });
-                    if (!is_button_clicked) {
-                        let formatted_amount = new Intl.NumberFormat().format(allocated_amount);
-                        frappe.show_alert({
-                            message: __(
-                                "Deposit amount <b>{0}</b> will be allocated.<br/>\
-                                Please verify Deposit Deductions section in tab Payment.<br/>\
-                                Then save the document to add the deduction amount in Items child table.",
-                                [formatted_amount]),
-                            indicator: "green"
-                        }, 20);
-                    }
-                }
-                // Refresh the child table
-                frm.refresh_field("deposits");
-                frm.dirty();
-            }
-        });
+    get_deposits: function(frm, is_button_clicked = true) {
+        erpnext_thailand.deposit_utils.get_deposits(frm, is_button_clicked);
     }
 });
